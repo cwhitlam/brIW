0b6334cb8e32bf661efdb1a07d44391263c89ac5
Chris Whitlam <chris.whitlam@infinityworks.com>
2019-09-17 19:53:24 +0100

    Add more table tests

--- test/test_table.py
+++ test/test_table.py
@@ -21,13 +21,25 @@ class TestTable(unittest.TestCase):
         table = Table("People", ["id", "name"], self.drinks)
 
         self.assertEqual(table_title.upper(), table.table_title)
-        self.assertEqual(self.drinks.values(), table.table_contents)
+        self.assertListEqual(list(self.drinks.values()), list(table.table_contents))
 
-"""
     def test__init__with_list(self):
+        table_title = "People"
 
+        coffee = Drink(0, "Coffee")
+        tea    = Drink(1, "Tea")
+
+        drinks = [coffee, tea]
+
+        table = Table("People", ["id", "name"], drinks)
+
+        self.assertEqual(table_title.upper(), table.table_title)
+        self.assertListEqual(drinks, table.table_contents)
+"""
     def test_calculate_table_width(self):
-
+        
+        table = Table("Drinks", ["id", "name"], drinks)
+        table._calculate_table_width()
 
     def test_calculate_column_widths(self):
 """
