4e15c39dc81cba98dac186e79de47e13035713fe
Chris Whitlam <chris.whitlam@infinityworks.com>
2019-09-17 11:46:21 +0100

    Configure tests

--- src/testing.py
+++
@@ -1,6 +0,0 @@
-from file_manager import File_Manager
-
-file_man = File_Manager()
-
-people = file_man.load_from_file("./src/stored_data/people.json")
-print(people)
---
+++ test/test_briw.py
@@ -0,0 +1,8 @@
+import sys
+import unittest
+sys.path.insert(1, "/home/chris/repos/miniproject/src/")
+from person import Person
+
+class TestBriw(unittest.TestCase):
+    def test_add_person(self):
+
