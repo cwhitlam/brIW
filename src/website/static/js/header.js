1866065ff1f6979bd6028b721b70488b8cbe5783
Chris Whitlam <chris.whitlam@infinityworks.com>
2019-10-01 16:37:20 +0100

    Added icons

--- src/website/static/js/header.js
+++ src/website/static/js/header.js
@@ -1,10 +1,9 @@
 const navBar    = document.querySelector("#nav-bar");
 const navBarBtn = document.querySelector('#menu-icon');
-navBarBtn.addEventListener("click", function(){
-    
-    if (navBar.style.display == "none") {
-        navBar.style.display = "block";
-    } else {
+function handleMenuClick(){
+    if (navBar.style.display == "block" ) {
         navBar.style.display = "none";
+    } else {
+        navBar.style.display = "block";
     }
-})
+}
