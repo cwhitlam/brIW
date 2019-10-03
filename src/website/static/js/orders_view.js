e969bfbeae842b3bd08f40987e5d22879fefd983
Chris Whitlam <chris.whitlam@infinityworks.com>
2019-10-03 21:27:21 +0100

    Add a new order modal

---
+++ src/website/static/js/orders_view.js
@@ -0,0 +1,18 @@
+//Modal handling
+var modal = document.getElementById("js-create-order-modal");
+var btn = document.getElementById("js-create-order-btn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
