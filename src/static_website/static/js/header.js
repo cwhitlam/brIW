f54cd1590363843fc847e41a932b247867f377f3
Chris Whitlam <chris.whitlam@infinityworks.com>
2019-10-08 17:11:55 +0100

    Replaced static website with 90s site

--- src/static_website/static/js/header.js
+++
@@ -1,9 +0,0 @@
-const navBar    = document.querySelector("#nav-bar");
-const navBarBtn = document.querySelector('#menu-icon');
-function handleMenuClick(){
-    if (navBar.style.display == "block" ) {
-        navBar.style.display = "none";
-    } else {
-        navBar.style.display = "block";
-    }
-}
